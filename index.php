ed6bb44cfa1e658575bda3a52e4e6951d606393d
JoseCarlosMarRangel <1930082@upv.edu.mx>
2022-02-23 21:52:15 -0600

    index apenas con nabvar

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wallhala</title>
+    <!--- Estilo de la barra de search-->
+    <link rel="stylesheet" href="./barradebusqueda/style.css">
+    <link rel="stylesheet" href="./styles/style.css">
+
+</head>
+
+<body>
+
+    <div class=header>
+        <h2 class="tittle"> WallGames</h2>
+    </div>
+
+    <?php
+    include "barradebusqueda/search.php";
+    ?>
+
+</body>
+
+</html>
