c7f28f15ea3494d222fd47fc894eb7f8b601d7d9
José Carlos Mar Rangel <68357760+JoseCarlosMarRangel@users.noreply.github.com>
2022-02-25 03:06:10 -0600

    Update search.php

--- barradebusqueda/search.php
+++ barradebusqueda/search.php
@@ -7,7 +7,12 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 <body>
+    <div class="nav-item">
+        <a href="#">Iniciar Sesion</a>
+        <a href="#">Registrarse</a>
+    </div>
     <div id="navlist">
+
         <!-- search bar right align -->
         <div class="search">
 
@@ -20,6 +25,29 @@
             </form>
         </div>
     </div>
+    <div class="cuerpo">
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit nostrum aperiam non rerum, obcaecati est autem
+            eos,
+            nam ratione excepturi, illum hic amet voluptatibus veritatis voluptates! Sed quis minima facere.</h1>
+    </div>
 </body>
+
+<footer>
+    <div class="footer">
+        <div class="footer-content">
+            <div class="footer-section about">
+                <h1 class="logo-text">
+                    <span>C</span>ourse
+                    <span>B</span>ook
+                </h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                    labore
+                    et dolore magna aliqua.
+                </p>
+                <div class="contact">
+                    <span><i class="fa fa-phone"></i>&nbsp;&nbsp;+1-123-456-7890</span>
+                    <span><i class="fa fa-envelope"></i>&nbsp;&nbsp;
+</footer>
 
 </html>
