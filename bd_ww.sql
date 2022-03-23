2d534d07fdb4c212f9e4fbf3aa635ae955dcf925
JoseCarlosMarRangel <1930082@upv.edu.mx>
2022-03-22 20:55:42 -0600

    cambio de tupla contraseña por pass

--- bd_ww.sql
+++ bd_ww.sql
@@ -1,4 +1,4 @@
-CREATE TABLE usuario(usuario varchar(30) NOT NULL PRIMARY KEY, contraseña varchar(30) NOT NULL, nombre varchar(50), apellido varchar(50), descripcion varchar(100), correo varchar(70));
+CREATE TABLE usuario(usuario varchar(30) NOT NULL PRIMARY KEY, pass varchar(30) NOT NULL, nombre varchar(50), apellido varchar(50), descripcion varchar(100), correo varchar(70));
 
 CREATE TABLE wallpaper(id int NOT NULL PRIMARY KEY AUTO_INCREMENT, titulo varchar(30) NOT NULL, descripcion varchar(100), categoria int NOT NULL, imagen varchar(50) NOT NULL, usuario varchar(30) NOT NULL);
 
