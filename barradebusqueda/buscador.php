c8c12f03759731c5b0a9b70df5f78f03320d2cd6
CarlosReyesPuga <56316968+CarlosReyesPuga@users.noreply.github.com>
2022-04-17 19:20:00 -0500

    Add files via upload

---
+++ barradebusqueda/buscador.php
@@ -0,0 +1,15 @@
+<?php
+
+	requiere '../login/conexion.php';
+
+	if (!isset($_POST['buscar'])) {
+		$_POST['buscar'] = "";
+		$buscar = $_POST['buscar'];
+	}
+
+	$buscar = $_POST['buscar'];
+
+	$SQL_READ = "SELECT imagen FROM wallpaper WHERE categoria LIKE '%.$buscar.%'";
+
+	$sql_query = mysql_query($conexion,$SQL_READ); 
+?>
